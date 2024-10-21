f87a7924c44ff6b933e405c768963f79a320c2a0
José <jose@gurmeo.com>
2024-10-21 16:45:32 +0200

    chore: add Winston for logging
    - Added Winston dependency for logging.
    - Updated package.json and package-lock.json.

--- dist/routes/zendeskRoutes.js
+++ dist/routes/zendeskRoutes.js
@@ -5,10 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const zendeskController_1 = require("../controllers/zendeskController");
-const zendeskAuthMiddleware_1 = require("../middlewares/zendeskAuthMiddleware");
 const router = express_1.default.Router();
-// Apply middleware
-router.use(zendeskAuthMiddleware_1.zendeskAuthMiddleware);
 // Define routes
 router.get('/categories', zendeskController_1.getCategories);
 router.get('/tickets', zendeskController_1.getTickets);
